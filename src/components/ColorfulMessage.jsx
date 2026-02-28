59da7d0a68d7d4422b234196725b9a657b80fc81
ioO7zn <guminergg@gmail.com>
2026-02-28 22:42:46 +0900

    Reactの基本を学ぶ終了

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,12 @@
+export const ColorfulMessage = (props) => {
+    const { color, children } = props;
+    console.log(props)
+    const contentStyle = {
+        color,
+        fontSize: "18px"
+      };
+
+      return (
+        <p style={contentStyle}>{children}</p>
+      )
+}
